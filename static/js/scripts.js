4c6bbb37688b3bd3c2cc1a720ab88c314285d27f
Cyril <kidvos@gmail.com>
2021-11-22 22:21:11 +0700

    хэдэр трах трахыч

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -8,8 +8,7 @@ rating.addEventListener("change", function (e) {
         method: 'POST',
         body: data
     })
-        .then(response => alert("Рейтинг установлен"))
-        .catch(error => alert("Ошибка"))
+        
 });
 
 
