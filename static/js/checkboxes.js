6bf3a7aced3ac69002fd5b4fb283afc663644844
Cyril Strone <kidvos@gmail.com>
2022-05-25 00:01:13 +0700

    Баги пропали, поиск не работает

--- static/js/checkboxes.js
+++ static/js/checkboxes.js
@@ -1,19 +1,40 @@
-var expanded = false;
-document.getElementById('checkboxes').style.display = "none";
-function showCheckboxes() 
-{
+let expanded = false;
 
-  var checkboxes = document.getElementById("checkboxes");
 
-    
-  if (!expanded) {
-  	document.getElementById('side-bar-script').style.height = "232px";
-    checkboxes.style.display = "block";
-    expanded = true;
-  } 
-  else {
-  	document.getElementById('side-bar-script').style.height = "100%";
-    checkboxes.style.display = "none";
-    expanded = false;
-  }
+function showCheckboxestwo() {
+    let checkboxestwo2 = document.getElementById('checkboxestwo');
+    let checkboxes1 = document.getElementById('checkboxes');
+
+
+    if (!expanded) {
+        document.getElementById('side-bar-script').style.height = "232px";
+        checkboxestwo2.style.display = "block";
+        checkboxes1.style.display = "none";
+        expanded = true;
+    } else {
+        document.getElementById('side-bar-script').style.height = "100%";
+        checkboxestwo2.style.display = "none";
+        checkboxes1.style.display = "none";
+        expanded = false;
+    }
+}
+
+
+
+function showCheckboxes() {
+    let checkboxes = document.getElementById('checkboxes');
+    let checkboxestwo = document.getElementById('checkboxestwo');
+
+
+    if (!expanded) {
+        document.getElementById('side-bar-script').style.height = "232px";
+        checkboxes.style.display = "block";
+        checkboxestwo.style.display = "none";
+        expanded = true;
+    } else {
+        document.getElementById('side-bar-script').style.height = "100%";
+        checkboxes.style.display = "none";
+        checkboxestwo.style.display = "none";
+        expanded = false;
+    }
 }
