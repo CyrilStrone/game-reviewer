a044531e187918ecbafc79818796b72b10450f41
Cyril Strone <kidvos@gmail.com>
2022-05-26 04:26:07 +0700

    Поиск сайта

--- static/js/checkboxes.js
+++ static/js/checkboxes.js
@@ -1,13 +1,22 @@
 let expanded = false;
 
 
+
+
+
 function showCheckboxestwo() {
+    let childyear = document.getElementById('w3layouts-box-list__year').getElementsByTagName('li').length;
+    let height = $('#checkbox_list_year').height();
     let checkboxestwo2 = document.getElementById('checkboxestwo');
     let checkboxes1 = document.getElementById('checkboxes');
 
+    height = Math.floor(childyear / 5) * height + 20 + 50 + 30;
+
+
 
     if (!expanded) {
-        document.getElementById('side-bar-script').style.height = "232px";
+        document.getElementById('side-bar-script').style.height = (height) + "px";
+        $('#side-bar-script').height(height);
         checkboxestwo2.style.display = "block";
         checkboxes1.style.display = "none";
         expanded = true;
@@ -25,9 +34,12 @@ function showCheckboxes() {
     let checkboxes = document.getElementById('checkboxes');
     let checkboxestwo = document.getElementById('checkboxestwo');
 
+    let childyear = document.getElementById('w3layouts-box-list__ganre').getElementsByTagName('li').length;
+    let height = $('#checkbox_list_year').height();
+    height = Math.ceil(childyear / 5) * height + 20 + 50 + 30;
 
     if (!expanded) {
-        document.getElementById('side-bar-script').style.height = "232px";
+        document.getElementById('side-bar-script').style.height = (height) + "px";
         checkboxes.style.display = "block";
         checkboxestwo.style.display = "none";
         expanded = true;
